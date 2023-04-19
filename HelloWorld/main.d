a707a6721da623c0030e87530b426cf5f438f76e
sistemkodlama <mm.clskn@gmail.com>
2023-04-19 12:28:45 +0300

    update

---
+++ HelloWorld/main.d
@@ -0,0 +1,8 @@
+import std.stdio;         // input/output işlemleri için gerekli kütüphanenin eklenmesi 
+                          // C/C++ 'daki #include ifadesi ile aynı işleve sahiptir
+                          // std klasöründeki stdio dosyasını ekle 
+
+// kodların işlenmeye başlandığı "başlangıç-noktası" - main fonksiyonu 
+void main() {
+    writeln("Hello World");     // writeln() fonksiyonu ile ekrana çıktı basılır
+}
